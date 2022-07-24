261615320dd96c20a293b9eef79da97c19f378d8
Paulo Guilherme <pauloguilherme@usp.br>
2022-07-23 22:12:59 -0300

    Add initial menu

---
+++ PauloGuilherme-d3-avaliacao/Program.cs
@@ -0,0 +1,46 @@
+﻿// Bcrypt
+namespace InitialMenu
+{
+    internal class Program
+    {
+        // Method to show the menu
+        static void ShowMenu(){
+            Console.WriteLine("\n-------MENU--------");
+            Console.WriteLine("1) Access system");
+            Console.WriteLine("2) Cancel operation");
+        }
+
+        // Method to take an action given an option
+        static void DoChoosedOption(string option){
+            switch(option)
+            {
+                case "1":
+                    Console.WriteLine("Opening system:");
+                    break;
+                case "2":
+                    Console.WriteLine("Finishing program");
+                    break;
+                default:
+                    Console.WriteLine("Please, choose a command from the menu:");
+                    break;
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string option;
+
+            do
+            {
+                ShowMenu();
+
+                option = Console.ReadLine();
+                
+                DoChoosedOption(option);
+
+            }while(option != "2");
+
+        }
+    }
+}
