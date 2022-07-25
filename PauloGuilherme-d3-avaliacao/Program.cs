b6d2d9f41fc6ba937e275ae2732860b892d442e8
Paulo Guilherme <pauloguilherme@usp.br>
2022-07-25 12:00:51 -0300

    Add utils file to organize methods from program

--- PauloGuilherme-d3-avaliacao/Program.cs
+++ PauloGuilherme-d3-avaliacao/Program.cs
@@ -1,8 +1,7 @@
-﻿// Bcrypt
-using Models;
-using Repositories;
+﻿using Models;
+using UM = Utils.UtilsMethods;
 
-namespace InitialMenu
+namespace Menu
 {
     internal class Program
     {
@@ -20,34 +19,50 @@ namespace InitialMenu
             Console.WriteLine("2) Turn off system");
         }
 
-        // Method to realize the login process:
-        static string?[] LoginProtocol(){
-            string? email, password;
-            string?[] DateAndHour, LoginInfo = new string[2];
-
-            Console.Write("Write your email:");
-            email = Console.ReadLine();
-        
-            Console.Write("Write your password:");
-            password = Console.ReadLine();
-
-            LoginInfo = UserRepository.CheckUserPassword(email, password);
-            DateAndHour = TakeCurrentDateAndHour();
-
-
-            if(LoginInfo[0] != null && LoginInfo[1] != null){
-                Console.WriteLine("Login executed with success. Showing new menu:\n");
-            }else{
-                Console.WriteLine("Username or password are wrong, please try again.");
+        // Method to take an action given an option in the main menu
+        static bool DoChoosedOption(string? option){
+            bool running = true;
+            if(option == null){
+                Console.WriteLine("Error: Option choosed is null");
+                return running;
             }
-            return LoginInfo;
-        }
+            switch(option)
+            {
+                case "1":
+                    string?[] LoginInfo = UM.LoginProtocol();
+                    if(LoginInfo[0] != null && LoginInfo[1] != null){
+                        UserAccess userAccess = new(LoginInfo);
+                        string[] FormatedDateAndHour = UM.TakeCurrentDateAndHour();
+                        userAccess.SaveUserAcessInfo(userAccess, FormatedDateAndHour);
+                        string? option2;
+                        bool running2;
 
-        // Method to take current date and hour
-        static string[] TakeCurrentDateAndHour(){
-            string[] DateAndHour = DateTime.UtcNow.ToString().Split(" ");
-            string[] FormatedDateAnHour = {DateAndHour[0], $"{DateAndHour[1]} {DateAndHour[2]}"};
-            return FormatedDateAnHour;
+                        do
+                        {
+                            ShowMenu2();
+
+                            option2 = Console.ReadLine();
+                            
+                            running2 = DoChoosedOption2(option2);
+
+                        }while(running2);
+                        
+                        FormatedDateAndHour = UM.TakeCurrentDateAndHour();
+                        userAccess.SaveUserLogOutInfo(userAccess, FormatedDateAndHour);
+                        running = !(option2 == "2"); 
+
+                    }
+                    break;
+                case "2":
+                    Console.WriteLine("Finishing program");
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Please, choose a command from the menu:");
+                    break;
+            }
+
+            return running;
         }
 
         // Method to take an action given an option in the second menu
@@ -76,63 +91,17 @@ namespace InitialMenu
             return running2;
         }
 
-        // Method to take an action given an option in the main menu
-        static bool DoChoosedOption(string? option){
-            bool running = true;
-            if(option == null){
-                Console.WriteLine("Error: Option choosed is null");
-                return running;
-            }
-            switch(option)
-            {
-                case "1":
-                    string?[] LoginInfo = LoginProtocol();
-                    if(LoginInfo[0] != null && LoginInfo[1] != null){
-                        UserAccess userAccess = new(LoginInfo);
-                        string[] FormatedDateAndHour = TakeCurrentDateAndHour();
-                        userAccess.SaveUserAcessInfo(userAccess, FormatedDateAndHour);
-                        string? option2;
-                        bool running2;
-
-                        do
-                        {
-                            ShowMenu2();
-
-                            option2 = Console.ReadLine();
-                            
-                            running2 = DoChoosedOption2(option2);
-
-                        }while(running2);
-                        
-                        FormatedDateAndHour = TakeCurrentDateAndHour();
-                        userAccess.SaveUserLogOutInfo(userAccess, FormatedDateAndHour);
-                        running = !(option2 == "2"); 
-
-                    }
-                    break;
-                case "2":
-                    Console.WriteLine("Finishing program");
-                    running = false;
-                    break;
-                default:
-                    Console.WriteLine("Please, choose a command from the menu:");
-                    break;
-            }
-
-            return running;
-        }
-
         //Main function
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
             bool running;
             string? option;
+
             do
             {
                 ShowMenu();
 
                 option = Console.ReadLine();
-                
+
                 running = DoChoosedOption(option);
 
             }while(running);
---
+++ PauloGuilherme-d3-avaliacao/Utils/UtilsMethods.cs
@@ -0,0 +1,36 @@
+using Repositories;
+
+namespace Utils
+{
+    internal class UtilsMethods
+    {
+        // Method to realize the login process:
+        public static string?[] LoginProtocol(){
+            string? email, password;
+            string?[] LoginInfo = new string[2];
+
+            Console.Write("Write your email:");
+            email = Console.ReadLine();
+        
+            Console.Write("Write your password:");
+            password = Console.ReadLine();
+
+            LoginInfo = UserRepository.CheckUserPassword(email, password);
+
+            if(LoginInfo[0] != null && LoginInfo[1] != null){
+                Console.WriteLine("Login executed with success. Showing new menu:\n");
+            }else{
+                Console.WriteLine("Username or password are wrong, please try again.");
+            }
+            return LoginInfo;
+        }
+
+        // Method to take current date and hour
+        public static string[] TakeCurrentDateAndHour(){
+            string[] DateAndHour = DateTime.UtcNow.ToString().Split(" ");
+            string[] FormatedDateAnHour = {DateAndHour[0], $"{DateAndHour[1]} {DateAndHour[2]}"};
+            return FormatedDateAnHour;
+        }
+
+    }
+}
